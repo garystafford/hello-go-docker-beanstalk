6eeb7bf4d13c67ee49de67f8f22b315ca7cf4495
Gary Stafford <gary.stafford@accenture.com>
2018-03-13 19:17:07 -0400

    Fix favicon

--- hello_world.go
+++ hello_world.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
 const (
-  port = ":3000"
+	port = ":3000"
 )
 
 var calls = 0
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-  calls++
-  fmt.Fprintf(w, "Hello, world! You have called me %d times.\n", calls)
+	calls++
+	fmt.Fprintf(w, "Hello, world! You have called me %d times.\n", calls)
 }
 
-func handlerICon(w http.ResponseWriter, r *http.Request) {}
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/favicon.ico")
+}
 
 func init() {
-  fmt.Printf("Started server at http://localhost%v.\n", port)
-  http.HandleFunc("/favicon.ico", handlerICon)
-  http.HandleFunc("/", helloWorld)
-  http.ListenAndServe(port, nil)
+	fmt.Printf("Started server at http://localhost%v.\n", port)
+	http.HandleFunc("/favicon.ico", faviconHandler)
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(port, nil)
 }
 
 func main() {}
